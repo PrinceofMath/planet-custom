43d17e6f8d49ca52edc2bb703244fb3f5cc62b8c
Jamoliddin Kodirov <princeofmath13@gmail.com>
2021-05-21 08:34:07 +0500

    test

--- planet_custom/tasks.py
+++ planet_custom/tasks.py
@@ -3,6 +3,14 @@ import sys
 import os
 import requests
 import time
+import frappe
+
+apikey = '92e11e515ad344b386c19f0a4dce232c'
+client = api.ClientV1(api_key=apikey)
+
+item_type = ["PSScene4Band"]
+asset_type = 'visual'
 
 def create_daily_image():
-    log('create daily image')
+    locations = frappe.db.get_list("Location")
+    print(locations)
